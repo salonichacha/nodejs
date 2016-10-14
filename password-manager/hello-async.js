2345b025c3f75e2a24ad8774972e7f87f61e31da
salonichacha <salonichacha92@gmail.com>
2016-10-13 19:25:25 -0700

    Aysnc function

--- password-manager/hello-async.js
+++ password-manager/hello-async.js
@@ -1,2 +1,17 @@
-console.log('1');
+
+setTimeout(function(){
+	console.log('1');
+}, 2000);
+
 console.log('2');
+
+console.log('Challange : ');
+
+function printIntwoSeconds(message){
+	setTimeout(function(){
+			console.log(message);
+	}, 2000);
+}
+
+
+printIntwoSeconds('Hello Async');
